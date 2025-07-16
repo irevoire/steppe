f021346518ee3f1528653e236193385c52506eb4
Tamo <tamo@meilisearch.com>
2025-07-17 01:39:11 +0200

    make utoipa optional

--- src/lib.rs
+++ src/lib.rs
@@ -68,7 +68,6 @@ use std::time::{Duration, Instant};
 
 use indexmap::IndexMap;
 use serde::Serialize;
-use utoipa::ToSchema;
 
 /// The main trait of the crate. That describes an unit of works.
 /// - It contains a name that can change over time.
@@ -350,18 +349,18 @@ macro_rules! make_atomic_progress {
 }
 
 /// The returned view of the progress.
-#[derive(Debug, Serialize, Clone, ToSchema)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+#[derive(Debug, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
-#[schema(rename_all = "camelCase")]
 pub struct ProgressView {
     pub steps: Vec<ProgressStepView>,
     pub percentage: f32,
 }
 
 /// The view of the individual steps.
-#[derive(Debug, Serialize, Clone, ToSchema)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+#[derive(Debug, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
-#[schema(rename_all = "camelCase")]
 pub struct ProgressStepView {
     pub current_step: Cow<'static, str>,
     pub finished: u64,
